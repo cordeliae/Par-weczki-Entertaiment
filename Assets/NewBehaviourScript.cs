e5b7bcb47c7cde1b672799d4c6eb01922463f105
FrancuzA <149786616+FrancuzA@users.noreply.github.com>
2023-11-06 16:47:56 +0100

    How v1.0.1
    test

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    public void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left *0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * 0.1f);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * 0.1f);
+        }
+
+       
+    }
+    
+}
+
